52073b25ae29b74f281ae3b50a8225e33b220701
m4teuss <mateuus.s.silva@gmail.com>
2021-08-19 14:42:02 -0300

    Adicionando comentario

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,5 +1,8 @@
 describe(" Tickets", () => {
 
+    // Acessando page
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
+    
+    // Validando Titulo
     it("has 'TICKETBOX' header's  heading", () => {});
 })
