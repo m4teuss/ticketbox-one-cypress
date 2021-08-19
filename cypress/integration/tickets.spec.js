0b4946cfe09de0d5b18574f458eea5e78f2fabdc
m4teuss <mateuus.s.silva@gmail.com>
2021-08-19 15:02:44 -0300

    Trabalhando com comandos it.only e cy.get().type

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,7 +2,32 @@ describe(" Tickets", () => {
 
     // Acessando page
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
+
+
+    it.only("Preencher campos do tipo texto", () => {
+
+        const firstName = "Samuca"
+        const lastName = "Thaylada"
+        
+
+        cy.get("#first-name").type(firstName)
+        cy.get("#last-name").type(lastName)
+        cy.get("#email").type('samuca.thaylada@gmail.com')
+        cy.get("#requests").type('mc')
+        cy.get("#signature").type( `${firstName} ${lastName}`)
+        
+
+
+
+
+    })
+
+
+
+    it("Validar Titulo", () => {});
+    })
+
+  
     
     // Validando Titulo
-    it("has 'TICKETBOX' header's  heading", () => {});
-})
+   
